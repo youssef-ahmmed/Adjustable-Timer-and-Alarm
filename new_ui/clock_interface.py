58b5909c3423f4abcee6f52928dfb5c4ab7eece7
Medhat Mohammed <medhatmohammed1612@gmail.com>
2023-12-24 11:16:23 +0200

    Add clock functionalliy

--- new_ui/clock_interface.py
+++ new_ui/clock_interface.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import sys
+from datetime import datetime
 
 from PyQt5.QtCore import Qt, QUrl
 from PyQt5.QtGui import QIcon, QDesktopServices
@@ -7,6 +8,8 @@ from PyQt5.QtWidgets import QApplication, QFrame, QHBoxLayout, QVBoxLayout
 from qmaterialwidgets import (NavigationItemPosition, CheckBox, FilledPushButton, MessageBox, setTheme, Theme, BottomNavMaterialWindow,
                               SubtitleLabel, setFont, TimePicker)
 from qmaterialwidgets import FluentIcon as FIF
+
+from serial_communication import SerialCommunication
 
 
 class ClockInterface(QFrame):
@@ -29,3 +32,40 @@ class ClockInterface(QFrame):
         self.vBoxLayout.addWidget(self.currentTimeCheck, 1, Qt.AlignCenter)
         self.vBoxLayout.addWidget(self.setClockButton, 1, Qt.AlignCenter)
         self.setObjectName('Clock')
+
+        self.serial = SerialCommunication.get_instance()
+
+
+        self.setClockButton.clicked.connect(self.get_clock_data)
+        self.currentTimeCheck.stateChanged.connect(self.toggle_editability)
+
+
+    def toggle_editability(self, state):
+        self.picker.setReadOnly(state == 2)
+
+    def get_clock_data(self):
+        print(f'get_clock_data')
+        if self.currentTimeCheck.isChecked():
+            hours = str(datetime.now().hour)
+            minutes = str(datetime.now().minute)
+        else:
+            print(self.picker.time)
+            hours = str(self.picker.time.hour())
+            minutes = str(self.picker.time.minute())
+
+        hours = '0' + hours if len(hours) == 1 else hours
+        minutes = '0' + minutes if len(minutes) == 1 else minutes
+
+        clock_time = hours + minutes + '00'
+
+        print(clock_time)
+
+        self.send_clock_data(clock_time)
+
+
+    def send_clock_data(self, clock_time):
+        print(f'send_clock_data {clock_time}')
+        self.serial.open_connection()
+        self.serial.write_data('1' + clock_time + '0')
+        self.serial.close_connection()
+        print(f'send_clock_data {clock_time}')
